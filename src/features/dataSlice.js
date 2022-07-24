c6032396d35673766a7674ef7d82c28c40769479
andcarter95 <and.carter95@gmail.com>
2022-07-24 13:16:42 -0500

    Activity complete

---
+++ src/features/dataSlice.js
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    objextId: 10245,
+    apiData: {},
+}
+
+export const dataSlice = createSlice({
+    name: 'data',
+    initialState,
+    reducers: {
+        setData: (state, action) => {
+            return {...state, apiData : action.payload}
+        },
+        clearData: () => {
+            return initialState
+        },
+        inputId: (state, action) => {
+            return { ...state, objectId: action.payload }
+        },
+        incrementId: (state) => {
+            return { ...state, objectId: state.objectId + 1 }
+        },
+        decrementId: (state) => {
+            return { ...state, objectId: state.objectId - 1 }
+        }
+    }
+})
+
+export const { setData, clearData, inputId, incrementId, decrementId } = dataSlice.actions
+
+export const fetchData = () => {
+    const dataThunk = async (dispatch, getState) => {
+        let state = getState()
+        const response = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${state.data.objectId}`)
+        const resData = await response.json()
+        console.log(resData)
+        dispatch(setData(resData))
+    }
+    return dataThunk
+}
+
+export default dataSlice.reducer
